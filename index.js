dc5f2fe93bc273806f46714e4ac22cab848e80f9
Daniel Liu <danielliuzy@fb.com>
2025-04-01 20:10:38 -0700

    update counter values

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ const io = new Server(httpServer, {
 let topScore = 0;
 let bottomScore = 0;
 
-let counter = 50;
+let counter = 0;
 
 io.on("connection", (socket) => {
   // TODO: send scores back to client
   socket.emit("state", { topScore, bottomScore, counter });
   socket.on("clickedTop", () => {
     counter++;
-    if (counter === 100) {
+    if (counter === 200) {
       topScore++;
-      counter = 50;
+      counter = 0;
       io.emit("won", "top");
     }
     io.emit("state", { topScore, bottomScore, counter });
   });
   socket.on("clickedBottom", () => {
     counter--;
-    if (counter === 0) {
+    if (counter === -200) {
       bottomScore++;
       counter = 0;
       io.emit("won", "bottom");
